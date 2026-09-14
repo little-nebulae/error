6a8194da7eef5880a267ec6fc6dd1243bff2608b
Ryan Luong <lumintriet@gmail.com>
2026-09-14 20:42:18 +0700

    feat(schemas): create NestedErrorObjectSchema.

--- src/schemas.ts
+++ src/schemas.ts
@@ -10,3 +10,18 @@ export function FlatErrorObjectSchema<TCode extends string>(code: TCode) {
 export type FlatErrorObject<TCode extends string> = z.infer<
   ReturnType<typeof FlatErrorObjectSchema<TCode>>
 >;
+
+export function NestedErrorObjectSchema<TCode extends string>(code: TCode) {
+  return z
+    .object({
+      name: z.string(),
+      message: z.string(),
+      cause: z.json(),
+      stack: z.string(),
+      code: z.literal(code),
+    })
+    .catchall(z.json());
+}
+export type NestedErrorObject<TCode extends string> = z.infer<
+  ReturnType<typeof NestedErrorObjectSchema<TCode>>
+>;
