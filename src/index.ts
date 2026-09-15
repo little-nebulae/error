5babb100185b18e1953c925df0ad50ef6867194d
Ryan Luong <lumintriet@gmail.com>
2026-09-15 09:23:21 +0700

    feat: define lib entry.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+export * from "@/classes/base";
+export * from "@/classes/unexpected";
+
+export * from "@/helpers/compose-error-message";
+export * from "@/helpers/is-error";
+
+export type * from "@/types";
