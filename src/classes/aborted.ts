bd47046e787ea93853c259dee961656e6dc5bd1c
Ryan Luong <lumintriet@gmail.com>
2026-09-18 20:00:47 +0700

    feat(classes): define AbortedErrorCause.

--- src/classes/aborted.ts
+++ src/classes/aborted.ts
@@ -6,9 +6,11 @@ import { BaseError } from "@/classes/base";
 export const ABORTED_ERROR_CODE = "ABORTED_ERROR";
 export type AbortedErrorCode = typeof ABORTED_ERROR_CODE;
 
+export type AbortedErrorCause = AbortError | { aborted: true; reason: unknown };
+
 export class AbortedError<TMeta extends BaseErrorMeta = null> extends BaseError<
   AbortedErrorCode,
-  AbortError,
+  AbortedErrorCause,
   TMeta
 > {
   readonly name = "AbortedError";
