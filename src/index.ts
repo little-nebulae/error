178f78b9322647374962e242e0ebb8149d039109
Ryan Luong <lumintriet@gmail.com>
2026-09-19 18:01:46 +0700

    feat: add abort and timeout to lib entry.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 export * from "@/classes/base";
 export * from "@/classes/unexpected";
+export * from "@/classes/aborted";
+export * from "@/classes/timed-out";
 
 export * from "@/helpers/compose-error-message";
 export * from "@/helpers/is-error";
+export * from "@/helpers/is-abort-error";
+export * from "@/helpers/is-timeout-error";
 
 export type * from "@/types";
