9b19fc45a0669a183546fa479964b5c17918c61e
Ryan Luong <lumintriet@gmail.com>
2026-09-19 09:30:29 +0700

    feat(helpers): change the type of isAbortError's param.

--- src/helpers/is-abort-error.ts
+++ src/helpers/is-abort-error.ts
@@ -1,16 +1,20 @@
 import type { Branded } from "@little-nebulae/type-utils";
 
+import { isError } from "@/helpers/is-error";
+
 export type AbortError = Branded<Error, "AbortError">;
 
-export function isAbortError(error: Error): error is AbortError {
-  if (error.name === "AbortError") {
-    return true;
-  }
-  if (
-    Object.hasOwn(error, "code") &&
-    (error as Error & { code: string }).code === "ABORT_ERR"
-  ) {
-    return true;
+export function isAbortError(value: unknown): value is AbortError {
+  if (isError(value)) {
+    if (value.name === "AbortError") {
+      return true;
+    }
+    if (
+      Object.hasOwn(value, "code") &&
+      (value as Error & { code: string }).code === "ABORT_ERR"
+    ) {
+      return true;
+    }
   }
   return false;
 }
