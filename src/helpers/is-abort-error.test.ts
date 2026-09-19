8076365d8c9b27478264bf6803726f3edecf6828
Ryan Luong <lumintriet@gmail.com>
2026-09-19 07:59:47 +0700

    test(is-abort-error): add another success case.

--- src/helpers/is-abort-error.test.ts
+++ src/helpers/is-abort-error.test.ts
@@ -30,4 +30,25 @@ describe("isAbortError function should succeed when", async () => {
     assert(isError(reason) === true);
     expect(isAbortError(reason)).toBe(true);
   });
+
+  test("the request is aborted with no reason provided after the fetch() call has been fulfilled but before the response body has been read", async () => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    const response = await fetch("https://example.com", { signal });
+    controller.abort();
+
+    try {
+      const text = await response.text();
+      console.log(text);
+      expect.fail("Expected await response.text() to throw");
+    } catch (error) {
+      assert(isError(error) === true);
+      expect(isAbortError(error)).toBe(true);
+    }
+
+    const { aborted, reason } = signal;
+    expect(aborted).toBe(true);
+    assert(isError(reason) === true);
+    expect(isAbortError(reason)).toBe(true);
+  });
 });
