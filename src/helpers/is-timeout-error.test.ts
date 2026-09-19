9744df6bf8dc6cb74b9271ec44ed241ae776349f
Ryan Luong <lumintriet@gmail.com>
2026-09-19 17:43:15 +0700

    test(is-timeout-error): write a success case.

---
+++ src/helpers/is-timeout-error.test.ts
@@ -0,0 +1,23 @@
+// oxlint-disable vitest/no-conditional-expect
+
+import { readFile } from "node:fs/promises";
+import { assert, describe, expect, test } from "vitest";
+
+import { isTimeoutError } from "@/helpers/is-timeout-error";
+
+// Success cases
+describe("isTimeoutError function should succeed when", async () => {
+  test("passed the cause of the error thrown by read file's timeout signal", async () => {
+    try {
+      const text = await readFile("package.json", {
+        encoding: "utf-8",
+        signal: AbortSignal.timeout(1),
+      });
+      console.log(text);
+      expect.fail("Expected readFile to throw");
+    } catch (error) {
+      assert(error instanceof Error);
+      expect(isTimeoutError(error.cause)).toBe(true);
+    }
+  });
+});
